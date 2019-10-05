9cbfec39724e8c8b100120efc06a8f27dbb2dd70
shizhanx <43192138+shizhanx@users.noreply.github.com>
2019-10-05 23:30:25 +1000

    create new artifacts
    tested by feeding aqours as $_POST['name'].
    gives an error when the name is exactly the same as any existing artifact.

--- backend/application/controllers/Artifact.php
+++ backend/application/controllers/Artifact.php
@@ -9,8 +9,23 @@ class Artifact extends CI_Controller
 		$this->load->model("Artifact_model");
 	}
 
-	public function view($type,$value){
+	public function get_artifact($type,$value){
 		$data['artifacts']=$this->Artifact_model->get_artifacts($type,$value);
 		$this->load->view('artifacts',$data);
+	}
+
+	public function create_artifact(){
+		$exist=$this->Artifact_model->get_artifacts('exact',$_POST['name']);
+		if (empty($exist)){
+			$this->Artifact_model->create_artifact();
+			$this->get_artifact('exact',$_POST['name']);
+		}else{
+			$this->error('Artifact already exist');
+		}
+	}
+
+	public function error($msg){
+		$data['error_msg']=$msg;
+		$this->load->view('error',$data);
 	}
 }
